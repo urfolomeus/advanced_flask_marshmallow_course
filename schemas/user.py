f43cfb2cffbed9710d5f8d0030dc43e8be8e26ed
Alan Gardner <urfolomeus@gmail.com>
2020-09-07 11:24:07 +0100

    Specify fields for dump or load only

--- schemas/user.py
+++ schemas/user.py
@@ -2,6 +2,10 @@ from marshmallow import Schema, fields
 
 
 class UserSchema(Schema):
+    class Meta:
+        dump_only = ("id",)
+        load_only = ("password",)
+
     id = fields.Int()
     username = fields.Str(required=True)
     password = fields.Str(required=True)
